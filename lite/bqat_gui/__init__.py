a01abb0c230123f7ed02c5cb06909909775ff08e
Liam Wu <l.wu@biometix.com>
2024-09-11 10:03:55 +1000

    fix: revert legacy version

---
+++ lite/bqat_gui/__init__.py
@@ -0,0 +1,15 @@
+TITLE = "BQAT"
+WEB_VERSION = "0.1.9 alpha"
+
+BQAT_API = "api:8848"
+SERVER = "0.0.0.0"
+
+FILE_TYPE = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".wsq",
+    ".bmp",
+    ".jp2",
+    # ".wav",
+]
